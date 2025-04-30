195b3a0fa8b6bdf214a574311fcfee6fe836c3d0
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-04-30 09:42:18 -0300

    feat: create collection of show products

--- app/Livewire/Products/Index.php
+++ app/Livewire/Products/Index.php
@@ -2,11 +2,20 @@
 
 namespace App\Livewire\Products;
 
+use App\Models\Product;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Computed]
+    public function products(): Collection
+    {
+        return Product::get();
+    }
+
     public function render(): View
     {
         return view('livewire.products.index');
--- resources/views/livewire/products/index.blade.php
+++ resources/views/livewire/products/index.blade.php
@@ -43,119 +43,52 @@
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider">Ações</th>
                 </tr>
                 </thead>
-                <tbody class="bg-[#242627] divide-y divide-[#242627]">
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0 h-10 w-10">
-                                <img src="{{ asset('/images/products/product-teste.jpg') }}" alt="Product" class="h-10 w-10 rounded-md">
-                            </div>
-                            <div class="ml-4">
-                                <div class="text-sm font-medium text-gray-200">
-                                    Apple AirPods Pro
+                @foreach ($this->products as $product)
+                    <tbody class="bg-[#242627] divide-y divide-[#242627]">
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <div class="flex-shrink-0 h-10 w-10">
+                                    <img src="{{ asset('/images/products/product-teste.jpg') }}" alt="Product" class="h-10 w-10 rounded-md">
                                 </div>
-                                <div class="text-sm text-gray-300 hidden sm:block">
-                                    SKU: AP-PRO-001
-                                </div>
-                            </div>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-200">R$ 189,90</div>
-                        <div class="text-sm text-gray-300 hidden sm:block">Custo: R$ 140,00</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell">
-                        <div class="text-sm text-gray-200">TechDrops Inc.</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Aprovado</span>
-                    </td>
-                    <td class="flex items-center mt-6 ml-5 whitespace-nowrap text-sm font-medium">
-                        <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300 mr-3">
-                            <x-svg.pencil-square class="w-5"/>
-                        </a>
-                        <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300">
-                            <x-svg.trash class="w-5" />
-                        </a>
-                    </td>
-                </tr>
-
-                <tbody class="bg-[#242627] divide-y divide-[#242627]">
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0 h-10 w-10">
-                                <img src="{{ asset('/images/products/product-teste.jpg') }}" alt="Product" class="h-10 w-10 rounded-md">
-                            </div>
-                            <div class="ml-4">
-                                <div class="text-sm font-medium text-gray-200">
-                                    Apple AirPods Pro
-                                </div>
-                                <div class="text-sm text-gray-300 hidden sm:block">
-                                    SKU: AP-PRO-001
+                                <div class="ml-4">
+                                    <div class="text-sm uppercase font-medium text-gray-200">
+                                        {{ $product->name }}
+                                    </div>
+                                    <div class="text-sm text-gray-300 hidden sm:block">
+                                        CÓD: {{ $product->product_code }}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-200">R$ 189,90</div>
-                        <div class="text-sm text-gray-300 hidden sm:block">Custo: R$ 140,00</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell">
-                        <div class="text-sm text-gray-200">TechDrops Inc.</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">Em análise</span>
-                    </td>
-                    <td class="flex items-center mt-6 ml-5 whitespace-nowrap text-sm font-medium">
-                        <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300 mr-3">
-                            <x-svg.pencil-square class="w-5"/>
-                        </a>
-                        <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300">
-                            <x-svg.trash class="w-5"/>
-                        </a>
-                    </td>
-                </tr>
-                </tbody>
-                <tbody class="bg-[#242627] divide-y divide-[#242627]">
-                <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="flex items-center">
-                            <div class="flex-shrink-0 h-10 w-10">
-                                <img src="{{ asset('/images/products/product-teste.jpg') }}" alt="Product" class="h-10 w-10 rounded-md">
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-200">
+                                R$ {{ $product->price_sale }}
                             </div>
-                            <div class="ml-4">
-                                <div class="text-sm font-medium text-gray-200">
-                                    Apple AirPods Pro
-                                </div>
-                                <div class="text-sm text-gray-300 hidden sm:block">
-                                    SKU: AP-PRO-001
-                                </div>
+                            <div class="text-sm text-gray-300 hidden sm:block">
+                                Custo: R$ {{ $product->price_product }}
                             </div>
-                        </div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-200">R$ 189,90</div>
-                        <div class="text-sm text-gray-300 hidden sm:block">Custo: R$ 140,00</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell">
-                        <div class="text-sm text-gray-200">TechDrops Inc.</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-200 text-red-800">
-                            Recusado
-                        </span>
-                    </td>
-                    <td class="flex items-center mt-6 ml-5 whitespace-nowrap text-sm font-medium">
-                        <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300 mr-3">
-                            <x-svg.pencil-square class="w-5"/>
-                        </a>
-                        <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300">
-                            <x-svg.trash class="w-5"/>
-                        </a>
-                    </td>
-                </tr>
-                </tbody>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell">
+                            <div class="text-sm text-gray-200">
+                                {{ $product->supplier }}
+                            </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                {{ $product->product_status }}
+                            </span>
+                        </td>
+                        <td class="flex items-center mt-6 ml-5 whitespace-nowrap text-sm font-medium">
+                            <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300 mr-3">
+                                <x-svg.pencil-square class="w-5"/>
+                            </a>
+                            <a href="#" class="text-yellow-600 hover:text-yellow-800 duration-300">
+                                <x-svg.trash class="w-5" />
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
             </table>
         </div>
     </div>
